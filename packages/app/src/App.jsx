52374491d101dc64370944b9772c315d269b032c
Glenn Penridge <glenn@eezeeco.com>
2019-11-17 12:58:21 +1000

    * Added linters. * Fixed small issues.

--- packages/app/src/App.jsx
+++ packages/app/src/App.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+/* eslint import/no-unresolved: [2, { ignore: ['webassembly$'] }] */
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import * as wasm from "webassembly";
+import * as wasm from 'webassembly';
 
-const App = ({ title }) =>
+const App = ({ title }) => (
   <div>
     <h1>{title}</h1>
     <button type="button" onClick={() => wasm.greet()}>
       Greet
     </button>
   </div>
+);
+
+App.propTypes = {
+  title: PropTypes.string,
+};
+
+App.defaultProps = {
+  title: 'Hello, ReactBris!',
+};
 
 export default App;
